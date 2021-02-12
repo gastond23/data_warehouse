07bbc9b6e0ce216efa6374860bb419d08c5106fe
gastond23 <gaston.dandre@gmail.com>
2021-02-11 22:30:49 -0300

    agregado de barra de interes a conctactos

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -1,1 +1,22 @@
-let createContactBtn = document.getElementById('create-contact-btn');
+let progressBar = document.getElementsByClassName('progress-bar');
+let greenBar = 'bg-success'
+let blueBar = 'bg-info';
+let yellowBar = 'bg-warning';
+let redBar = 'bg-danger';
+let value;
+
+if (progressBar.length > 0) {
+    for(let bar of progressBar){
+        value = bar.getAttribute('aria-valuenow');
+        bar.style.width = value + "%";
+        if (value <= 25) {
+            bar.classList.add(greenBar);
+        } else if (value > 25 && value <= 50){
+            bar.classList.add(blueBar);
+        } else if (value > 50 && value <= 75){
+            bar.classList.add(yellowBar);
+        } else if (value > 75) {
+            bar.classList.add(redBar);
+        }
+    }
+}
