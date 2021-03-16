fd7af9abda50de51b262b56f53b4e7a1592ab9bf
gastond23 <gaston.dandre@gmail.com>
2021-03-16 16:16:45 -0300

    checkbox funcionando, iniciando btn delete all

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -6,8 +6,27 @@ let redBar = 'bg-danger';
 let value;
 let inputId = document.getElementById('inputId');
 let formContenedor = document.getElementsByClassName('form-contenedor');
-
+let tableBody = document.getElementById('table-body');
 let contactId;
+let checkBoxAll = document.getElementById('allCheck');
+let checkBoxOne = document.getElementsByClassName('form-check-input');
+let rows = document.getElementsByClassName('rows');
+let arrayId = [];
+let uniqueArray;
+let contador = document.getElementById('contact-count');
+let countContainer = document.getElementsByClassName('contador');
+let btnContainer = document.getElementsByClassName('btn-cont');
+
+
+document.querySelectorAll('.form-check-input').forEach(item => {
+    item.addEventListener('click', e => {
+        if (e.target.id != 'allCheck') {
+            e.target.checked = false;
+            let value = e.target.value;
+            checkBoxValues(value);
+        }
+    })
+})
 
 if (progressBar.length > 0) {
     for (let bar of progressBar) {
@@ -51,3 +70,82 @@ function deleteContact(id) {
             console.log(err);
         })
 }
+
+tableBody.addEventListener('click', (e) => {
+    let valueId = e.target.title;
+    checkBoxValues(valueId);
+})
+
+function checkBoxValues(value) {
+    let valueId = value;
+    for (let i = 0; i < checkBoxOne.length; i++) {
+        if (valueId == checkBoxOne[i].value) {
+            if (!checkBoxOne[i].checked) {
+                checkBoxOne[i].checked = true;
+                arrayId.push(valueId);
+                uniqueArray = arrayId.filter((item, index) => {
+                    return arrayId.indexOf(item) === index;
+                });
+                arrayId = uniqueArray;
+                checkBoxAll.value = arrayId;
+                rows[i].classList.add('table-primary');
+            } else {
+                checkBoxOne[i].checked = false;
+                rows[i].classList.remove('table-primary');
+                for (let i = 0; i < arrayId.length; i++) {
+                    if (arrayId[i] == valueId) {
+                        arrayId.splice(i, 1);
+                        checkBoxAll.value = arrayId;
+                    }
+                }
+            }
+        }
+    }
+    checkStatusAll();
+}
+
+function checkStatusAll() {
+    if ((arrayId.length + 1) < checkBoxOne.length && arrayId.length > 0) {
+        checkBoxAll.indeterminate = true;
+        countContainer[0].classList.add('active-count');
+        btnContainer[0].classList.add('active-count');
+
+    } else if (checkBoxOne.length == (arrayId.length + 1)) {
+        checkBoxAll.indeterminate = false;
+        checkBoxAll.checked = true;
+    } else if (arrayId.length == 0) {
+        checkBoxAll.indeterminate = false;
+        checkBoxAll.checked = false;
+        countContainer[0].classList.remove('active-count');
+        btnContainer[0].classList.remove('active-count');
+    }
+    contador.innerHTML = `Contactos: ${arrayId.length}`;
+}
+
+checkBoxAll.addEventListener('click', () => {
+    arrayId = [];
+    if (checkBoxAll.checked) {
+        for (let i = 1; i < checkBoxOne.length; i++) {
+            checkBoxOne[i].checked = true;
+            countContainer[0].classList.add('active-count');
+            btnContainer[0].classList.add('active-count');
+            let uniqueValue = checkBoxOne[i].value;
+            arrayId.push(uniqueValue);
+        }
+        for (let i = 0; i < rows.length; i++) {
+            rows[i].classList.add('table-primary');
+        }
+    } else {
+        for (let i = 1; i < checkBoxOne.length; i++) {
+            checkBoxOne[i].checked = false;
+            countContainer[0].classList.remove('active-count');
+            btnContainer[0].classList.remove('active-count');
+            arrayId = [];
+        }
+        for (let i = 0; i < rows.length; i++) {
+            rows[i].classList.remove('table-primary');
+        }
+    }
+    checkBoxAll.value = arrayId;
+    contador.innerHTML = `Contactos: ${arrayId.length}`;
+})
